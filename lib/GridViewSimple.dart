b90c0867dc7907fd7c3befa2dea4511deb9f2f28
luhenchang <1276998208@qq.com>
2018-07-04 14:15:35 +0800

    上传哦

--- lib/GridViewSimple.dart
+++ lib/GridViewSimple.dart
@@ -14,28 +14,63 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
         home: new Scaffold(
             body: new Container(
-      child: new GridView.builder(
-        gridDelegate: new SliverGridDelegateWithMaxCrossAxisExtent(
-            mainAxisSpacing: 11.0, maxCrossAxisExtent: 210.0),
-        padding: new EdgeInsets.only(left: 20.0, right: 20.0),
-        itemBuilder: (BuildContext context, int index) {
-
-          return Container(
-            margin: new EdgeInsets.only(top: 5.0),
-            child: new Card(
-              child: Container(
-                child: new Image.network(
-                  "http://img.alicdn.com/imgextra/i4/1069988681/TB277mCgXooBKNjSZPhXXc2CXXa_!!1069988681.jpg",
-                  fit: BoxFit.fill,
+      child: new Column(
+        children: <Widget>[
+          new Expanded(
+            child: new Stack(
+              alignment: Alignment(1.0, 1.0),
+              children: <Widget>[
+                new GridView.builder(
+                  gridDelegate: new SliverGridDelegateWithMaxCrossAxisExtent(
+                    childAspectRatio: 200/280,
+                      mainAxisSpacing: 11.0, maxCrossAxisExtent: 210.0),
+                  padding: new EdgeInsets.only(left: 20.0, right: 20.0),
+                  itemBuilder: (BuildContext context, int index) {
+                    return index < 0
+                        ? Container(
+                            margin: new EdgeInsets.only(top: 5.0),
+                            child: new Card(
+                              child: Container(
+                                child: new Image.network(
+                                  "http://img.alicdn.com/imgextra/i4/1069988681/TB277mCgXooBKNjSZPhXXc2CXXa_!!1069988681.jpg",
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                            ),
+                            width: 100.0,
+                            height: 300.0,
+                            decoration: new BoxDecoration(color: Colors.teal),
+                          )
+                        : index == 11 || index == 10
+                            ? new Container(
+                                child: new Text('第二布局！'),
+                                color: Colors.teal,
+                              )
+                            : new Container(
+                                margin: new EdgeInsets.only(top: 5.0),
+                                child: new Card(
+                                  child: Container(
+                                    child: new Image.network(
+                                      "http://img.alicdn.com/imgextra/i4/1069988681/TB277mCgXooBKNjSZPhXXc2CXXa_!!1069988681.jpg",
+                                      fit: BoxFit.fill,
+                                    ),
+                                  ),
+                                ),
+                                width: 100.0,
+                                height: 300.0,
+                                decoration:
+                                    new BoxDecoration(color: Colors.teal),
+                              );
+                  },
+                  itemCount: 20,
                 ),
-              ),
+                new Container(
+                  child: new Text('我能咖妃'),
+                ),
+              ],
             ),
-            width: 100.0,
-            height: 300.0,
-            decoration: new BoxDecoration(color: Colors.teal),
-          );
-        },
-        itemCount: 20,
+          ),new Container(child: new Text('我爱你啊'),width: 600.0,height: 200.0,color: Colors.teal,)
+        ],
       ),
     )));
   }
--- lib/flutter_intent/NetDataPager.dart
+++ lib/flutter_intent/NetDataPager.dart
@@ -5,7 +5,9 @@ import 'package:flutter/material.dart';
 
 class NetDataPager extends StatefulWidget {
   final index;
-  NetDataPager({Key key,this.index}):super(key:key);
+
+  NetDataPager({Key key, this.index}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() {
     return new NetDataPagerState(index);
@@ -15,6 +17,7 @@ class NetDataPager extends StatefulWidget {
 class NetDataPagerState extends State<NetDataPager> {
   List widgets = [];
   int index;
+
   NetDataPagerState(this.index);
 
   @override
@@ -31,6 +34,7 @@ class NetDataPagerState extends State<NetDataPager> {
       widgets = json.decode(response.body);
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -49,7 +53,7 @@ class NetDataPagerState extends State<NetDataPager> {
                   },
                 ),
                 new Container(
-                  child: Text(index==0?'网络交互学习':'更新中'),
+                  child: Text(index == 0 ? '网络交互学习' : '更新中'),
                   margin: new EdgeInsets.only(left: 70.0),
                 ),
               ],
@@ -58,47 +62,55 @@ class NetDataPagerState extends State<NetDataPager> {
         ),
         body: new Container(
             color: Colors.teal,
-            child: ListView.builder(
-                itemCount:index==0?widgets.length:1,
-                itemBuilder: (BuildContext context, int position) {
-                  return getRow(position);
-                })),
+            child: widgets.length == 0
+                ? new Center(
+                    child: CircleAvatar(
+                      child: new CircularProgressIndicator(backgroundColor: Colors.white,),
+                    ),
+                  )
+                : ListView.builder(
+                    itemCount: index == 0 ? widgets.length : 1,
+                    itemBuilder: (BuildContext context, int position) {
+                      return getRow(position);
+                    })),
       ),
     );
   }
 
   Widget getRow(int position) {
-    return index==0?Container(
-        child:new Stack(
-            alignment: Alignment(1.0, 1.0),
-            children: <Widget>[
-              new Card(
-                margin: new EdgeInsets.all(15.0),
-                color: Colors.teal,
-                child: new Center(
-                  child: new Image.asset(
-                    'images/haha.png',
-                    width: 700.0,
-                    height: 200.0,
-                    fit: BoxFit.fitWidth,
-                  ),
-                ),
-              ),
-              new Container(
-                margin: new EdgeInsets.all(12.0),
-                child: new Card(
-                  child: new Container(
-                    color: Colors.white,
-                    width: 700.0,
-                    height: 50.0,
-                    child: new Center(
-                      child: new Text("Row ${widgets[position]["title"]}"),
+    return index == 0
+        ? Container(
+            child: new Stack(
+              alignment: Alignment(1.0, 1.0),
+              children: <Widget>[
+                new Card(
+                  margin: new EdgeInsets.all(15.0),
+                  color: Colors.teal,
+                  child: new Center(
+                    child: new Image.asset(
+                      'images/haha.png',
+                      width: 700.0,
+                      height: 200.0,
+                      fit: BoxFit.fitWidth,
                     ),
                   ),
                 ),
-              )
-            ],
-          ),
-    ):new Text('更新中...');
+                new Container(
+                  margin: new EdgeInsets.all(12.0),
+                  child: new Card(
+                    child: new Container(
+                      color: Colors.white,
+                      width: 700.0,
+                      height: 50.0,
+                      child: new Center(
+                        child: new Text("Row ${widgets[position]["title"]}"),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        : new Text('更新中...');
   }
 }
